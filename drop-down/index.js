1b86c1a94b5b3b259bd1d5779eb150f61315738e
PsammystPrime <samuelprime384@gmail.com>
2023-12-27 11:51:32 +0300

    Add dark mode functionality

--- drop-down/index.js
+++ drop-down/index.js
@@ -1,28 +1,41 @@
-const btn=document.querySelector('.menu')
-const menus=document.querySelector('article')
+const btn=document.querySelector('.menu');
+const menus=document.querySelector('article');
 
 btn.addEventListener('click',()=>{
-    menus.textContent = ''
-    const items = document.createElement('div')
-    items.className = 'items'
-    const profile = document.createElement('h3')
-    profile.textContent = 'Profile'
-    const links = document.createElement('h3')
-    links.textContent = 'Links'
-    const settings = document.createElement('h3')
-    settings.textContent = 'Settings'
-    const about = document.createElement('h3')
-    about.textContent = 'About'
-    const dark = document.createElement('h3')
-    dark.textContent = 'Dark'
-    const log = document.createElement('h3')
-   log.textContent = 'Log out'
+    if (menus.textContent === '') {
+        menus.textContent = '';
+        const items = document.createElement('div');
+        items.className = 'items';
+        const profile = document.createElement('h3');
+        profile.textContent = 'Profile';
+        const links = document.createElement('h3');
+        links.textContent = 'Links';
+        const settings = document.createElement('h3');
+        settings.textContent = 'Settings';
+        const about = document.createElement('h3');
+        about.textContent = 'About';
+        const dark = document.createElement('h3');
+        dark.textContent = 'Dark';
+        dark.className = 'dark';
+        const log = document.createElement('h3');
+       log.textContent = 'Log out';
+    
+       items.appendChild(profile);
+       items.appendChild(links);
+       items.appendChild(settings);
+       items.appendChild(about);
+       items.appendChild(dark);
+       items.appendChild(log);
+       menus.appendChild(items);
 
-   items.appendChild(profile)
-   items.appendChild(links)
-   items.appendChild(settings)
-   items.appendChild(about)
-   items.appendChild(dark)
-   items.appendChild(log)
-   menus.appendChild(items)
-})
+       dark.addEventListener('click', ()=>{
+        let element = document.body;
+        element.classList.toggle('dark');
+       });
+    } else {
+        menus.textContent = '';
+    };
+
+        
+});
+       
