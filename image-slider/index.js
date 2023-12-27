115b2ca8a5795c8d3f65201a4ef5dea1c1e2e576
PsammystPrime <samuelprime384@gmail.com>
2023-12-27 15:48:11 +0300

    Add css transition

--- image-slider/index.js
+++ image-slider/index.js
@@ -7,10 +7,14 @@ buttons.forEach(button =>{
         const slides = button.closest('[data-carousel]').querySelector('[data-slides]')
          const active =slides.querySelector('[data-active]')
          let newIndex =  [...slides.children].indexOf(active) + offset
-
+        if (newIndex < 0) {
+            newIndex = slides.children.length -1
+        }
+        if (newIndex >= slides.children.length) {
+            newIndex = 0
+        }
          slides.children[newIndex].dataset.active = true
          delete active.dataset.active
-        console.log('hey')
     });
 } 
-)
+);
