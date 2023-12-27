180568b0cb902ea5e4cc4189d169b62b194f824e
PsammystPrime <samuelprime384@gmail.com>
2023-12-27 13:07:53 +0300

    Update index.js

---
+++ image-slider/index.js
@@ -0,0 +1,11 @@
+const prev = document.querySelector('prev')
+const next = document.querySelector('next')
+const carousel = document.querySelector('carousel')
+
+next.addEventListener('click',()=>{
+
+});
+
+next.addEventListener('click',()=>{
+    
+});
